54e281a733b22bc72ab52ea4e984ed71d6a26569
Danilo Pereira <danilo4web@gmail.com>
2024-06-30 18:10:14 -0300

    Add migrations to create blog project tables

---
+++ config/Migrations/20240630201308_CreatePostLikes.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreatePostLikes extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('post_likes');
+        $table->addColumn('post_id', 'integer')
+            ->addColumn('user_id', 'integer')
+            ->addForeignKey('post_id', 'posts', 'id', ['delete'=> 'NO_ACTION', 'update'=> 'NO_ACTION'])
+            ->addForeignKey('user_id', 'users', 'id', ['delete'=> 'NO_ACTION', 'update'=> 'NO_ACTION'])
+            ->create();
+    }
+}
